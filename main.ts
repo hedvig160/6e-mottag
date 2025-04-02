53b7729e511382c2000fd1edfbc37e3e5a8a3218
hedvig160 <hedvig.kumblad@elevmail.stockholm.se>
2025-04-02 18:02:00 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else if (receivedNumber == 2) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else if (receivedNumber == 3) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
+basic.forever(function () {
+    radio.setGroup(37)
+})
